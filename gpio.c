fccce3e5d0e12bd62e8a294310d0c1a5dbd7d5b3
maemo <maemo@maemo-TomXue>
2011-10-04 16:26:39 +0100

    better code style, but doesn't work of LED control(GPIO150) on BB-XM-00 RevC

--- gpio.c
+++ gpio.c
@@ -14,8 +14,36 @@
 
 #define INT *(volatile unsigned int*)
 
+/*
+ * IEN  - Input Enable
+ * IDIS - Input Disable
+ * PTD  - Pull type Down
+ * PTU  - Pull type Up
+ * DIS  - Pull type selection is inactive
+ * EN   - Pull type selection is active
+ * M0   - Mode 0
+ */
+
+#define IEN     (1 << 8)
+
+#define IDIS    (0 << 8)
+#define PTU     (1 << 4)
+#define PTD     (0 << 4)
+#define EN      (1 << 3)
+#define DIS     (0 << 3)
+
+#define M0      0
+#define M1      1
+#define M2      2
+#define M3      3
+#define M4      4
+#define M5      5
+#define M6      6
+#define M7      7 
+
 void *map_base;
 int n,fd;
+unsigned int padconf;
 
 int main(int argc,char *argv[])
 {
@@ -27,17 +55,21 @@ int main(int argc,char *argv[])
     printf("fd=%d\n",fd);
 
     map_base = mmap(0,0x200,PROT_READ | PROT_WRITE,MAP_SHARED,fd,GPIO_BASE);
+    padconf = INT(map_base+GPIO_OFFSET);
+    padconf &= 0xffff0000;
     printf("map_base=%p\n",map_base);
     while(1){
 	if(strcmp(argv[1], "high") == 0)
        		{
-		INT(map_base+GPIO_OFFSET) = 0x100ffff; 	//original setting readout is 0x100,0004. set high. 
-		printf("high - The register value is set to: 0x%x\n", INT(map_base+GPIO_OFFSET));
+		padconf |= (EN | PTU | M4);
+		INT(map_base+GPIO_OFFSET) = padconf;
+		printf("high - The register value is set to: 0x%x\n", padconf);
 		}
 	else if(strcmp(argv[1], "low") == 0)
 		{
-		INT(map_base+GPIO_OFFSET) = 0x1000000;	//set low
-		printf("low - The register value is set to: 0x%x\n", INT(map_base+GPIO_OFFSET));
+		padconf |= (EN | PTD | M4);
+		INT(map_base+GPIO_OFFSET) = padconf;
+		printf("low - The register value is set to: 0x%x\n", padconf);
 		}
         usleep(10);
     }
